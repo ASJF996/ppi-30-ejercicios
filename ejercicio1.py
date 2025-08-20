c524112bbcdb0bc4076ab32b29f101c936bfc825
Francisco Alv <pacoalvarez.0196@gmail.com>
2025-08-19 21:22:06 -0600

    primer commit

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,8 @@
-a
+
+class Galleta:
+  def  __init__(self,nombre,forma):
+    self.nombre=nombre
+    self.forma=forma
+
+  def hornear(self):
+    print(f"Esta {self.nombre} ha sido horneada en forma de {self.forma}")
